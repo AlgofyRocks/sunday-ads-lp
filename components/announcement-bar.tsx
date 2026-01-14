6e06bd615d8a02c121c23ff55931db4b4727ab9a
AlgofyRocks <152904531+AlgofyRocks@users.noreply.github.com>
2026-01-14 09:06:17 -0300

    Update announcement-bar.tsx

--- components/announcement-bar.tsx
+++ components/announcement-bar.tsx
@@ -1,7 +1,7 @@
 export default function AnnouncementBar() {
   return (
     <div className="bg-[#ffa047] flex items-center justify-center py-2 font-bold text-[#153d50] uppercase text-xs md:text-md">
-      <p>Subscribe and Save 20% | Free shipping over $100!</p>
+      <p>Survive Dry January! Buy one, get one FREE now.</p>
     </div>
   );
 }
